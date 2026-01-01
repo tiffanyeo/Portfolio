a4bbd512fba596934cab56ec96699c6f1da65503
tiffanystud <tiffanylarsson.stud@gmail.com>
2026-01-01 19:27:12 +0100

    Import section render functions and navbar in main.js
    Added imports for navbar initialization and various section render functions in main.js to modularize and prepare for section rendering. No functional changes yet, as initApp remains unimplemented.

--- js/main.js
+++ js/main.js
@@ -33,10 +33,28 @@ initFooter();
     * contact
 */
 
+// Navbaar
+import { initNavbar } from './navbar.js';
 
+// Sections
+import { renderHero } from "./components/hero.js"; 
+import { renderAbout } from "./components/about.js"; 
+import { renderFunlines } from "./components/funlines.js"; 
+import { renderWhyMe } from "./components/whyme.js"; 
+import { renderHowIWork } from "./components/howiwork.js"; 
+import { renderSkills } from "./components/skills.js"; 
+import { renderSelectedWork } from "./components/selectedwork.js"; 
+import { renderProjects } from "./components/projects.js"; 
+import { renderEducation } from "./components/education.js"; 
+import { renderContact } from "./components/contact.js";
 
 function initApp() {
-    /* run imported funcs */
+
+
+
+
+
+
 }
 
 // When file is loaded, run initApp()
