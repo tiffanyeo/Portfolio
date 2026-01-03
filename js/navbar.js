927be94d81b62583c11770c89619f27fd9aaa769
tiffanystud <tiffanylarsson.stud@gmail.com>
2026-01-03 15:09:22 +0100

    Update layour
    Update for initial change of styling/layout

--- js/navbar.js
+++ js/navbar.js
@@ -30,13 +30,13 @@ export function initNavbar() {
             <ul class="navbar__links">
                 <li><a href="#hero">Home</a></li>
                 <li><a href="#about">About</a></li>
-                <li><a href="#funlines">Fun Lines</a></li>
+                <li><a class="displayNoneLink" href="#funlines">Fun Lines</a></li>
                 <li><a href="#whyme">Why Me</a></li>
-                <li><a href="#howiwork">How I Work</a></li>
+                <li><a class="displayNoneLink" href="#howiwork">How I Work</a></li>
                 <li><a href="#skills">Skills</a></li>
-                <li><a href="#selectedwork">Selected Work</a></li>
+                <li><a class="displayNoneLink" href="#selectedwork">Selected Work</a></li>
                 <li><a href="#projects">Projects</a></li>
-                <li><a href="#education">Education</a></li>
+                <li><a hclass="displayNoneLink" ref="#education">Education</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
